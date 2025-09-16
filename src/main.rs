3f3b9d3a213d747c71a3b4fc4240b1695fb02c59
psatomas <psatomas@gmail.com>
2025-09-16 18:12:07 -0300

    refactor: organize code into lodging module and utils module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use traits::lodging::{AirBnB, Hotel, Accommodation, Description};
+use traits::utils;
+
 fn main() {
-    println!("Hello, world!");
+    let mut hotel = Hotel::new(String::from("The Luxe"));
+    println!("{}", hotel.summarize());
+    hotel.book("Dana", 3);
+
+    let mut airbnb = AirBnB::new("Parker");
+    println!("{}", airbnb.get_description());
+    utils::book_for_one_night(&mut airbnb, "Dan");
+
+    utils::mix_and_match(&mut hotel, &mut airbnb, "Phil");
 }
