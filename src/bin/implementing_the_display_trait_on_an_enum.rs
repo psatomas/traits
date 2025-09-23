a3063d4cdba035310c10644681085deb6033b8ba
psatomas <psatomas@gmail.com>
2025-09-23 08:49:42 -0300

    feat: add AppleType enum and Display implementations

---
+++ src/bin/implementing_the_display_trait_on_an_enum.rs
@@ -0,0 +1,42 @@
+use std::fmt::{Display, Formatter, Result};
+
+enum AppleType {
+    RedDelicious,
+    GrannySmith,
+}
+
+impl Display for AppleType {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> Result {
+        match self {
+            AppleType::RedDelicious => write!(formatter, "Red Delicious"),
+            AppleType::GrannySmith => write!(formatter, "Granny Smith"),
+            
+        }
+    }    
+}
+
+struct Apple {
+    kind: AppleType,
+    price: f64,
+}
+
+impl Display for Apple {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> Result {
+        write!(formatter, "{} for {}", self.kind, self.price)
+    }
+}
+
+fn main() {
+    let lunch_snack = Apple {
+        kind: AppleType::GrannySmith,
+        price: 1.04,
+    };
+
+    let dinner_snack = Apple {
+        kind: AppleType::RedDelicious,
+        price: 1.15,
+    };
+
+    println!("{}", lunch_snack);
+    println!("{}", dinner_snack);
+}
